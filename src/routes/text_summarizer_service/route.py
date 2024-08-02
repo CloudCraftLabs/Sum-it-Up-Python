a18249081131e4a3ff78729c3223e53adf3df2f3
karan-kap00r <kaykay9464769@gmail.com>
2024-08-02 12:46:49 +0530

    Update route.py

--- src/routes/text_summarizer_service/route.py
+++ src/routes/text_summarizer_service/route.py
@@ -9,5 +9,5 @@ router.add_api_route('/generate-summary-via-url/', service.generate_summary_via_
 router.add_api_route('/generate-summary-via-text/', service.generate_summary_via_text_service, methods=["POST"])
 router.add_api_route('/generate-summary-flowchart/', service.generate_summary_flowchart_service, methods=["POST"])
 router.add_api_route('/download-pdf/', service.download_pdf, methods=["POST"])
-router.add_api_route('/text-to-speech/', service.text_to_speech_service, methods=["POST"])
+# router.add_api_route('/text-to-speech/', service.text_to_speech_service, methods=["POST"])
 router.add_api_route('/search-about/', service.search_about_service, methods=["POST"])
